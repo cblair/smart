49d0a4b3808c3344b6fb9c4088c6648e1bb1b37b
cblair <ecodata.cnr.uidaho@gmail.com>
2014-02-08 13:55:16 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smart::Application.config.secret_key_base = '2f2ce9d6df833e046cd14fc4c73f8f24b205fd082b2bfa1e3e181a24a7b87470a9eeca8167d1f411851055bfd90c687845b6b869e5a6d10793e27d6afefd1a69'
