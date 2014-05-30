cdddfa6142624cf12b043243edf16f7477ac7c00
cblair <ecodata.cnr.uidaho@gmail.com>
2014-05-29 22:08:13 -0700

    adding info to tweets

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -5,17 +5,14 @@ jQuery(function($) {
 
         //$('div.rider-alerts-content').hide();
         $(this).find('div.home-box-content').animate({opacity: 0}, 5);
-        console.log("TS7");
 
         $("div.home-box").hover(
             //On.
             function() {
                 //$(this).find('div.home-box-content').fadeIn(500);
                 $(this).find('div.home-box-content').animate({opacity: 100}, 500);
-                console.log("TS11");
             //Off.
             }, function() {
-                console.log("TS13");
                 //$(this).find('div.home-box-content').fadeOut(500);
                 $(this).find('div.home-box-content').animate({opacity: 0}, 500);
             }
