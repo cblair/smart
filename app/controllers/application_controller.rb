9f3281004ef37446f7ae952d31fde4944650e0f6
cblair <ecodata.cnr.uidaho@gmail.com>
2014-06-15 19:20:03 -0700

    reverting Facebook login to Devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,9 @@ class ApplicationController < ActionController::Base
 
    private
 
-   def current_user
-     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-   end
-   helper_method :current_user
+   #TODO: re-enable with Facebook login support
+   #def current_user
+   #  @current_user ||= User.find(session[:user_id]) if session[:user_id]
+   #end
+   #helper_method :current_user
 end
